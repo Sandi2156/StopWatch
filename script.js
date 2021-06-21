7b8775eee649e915120f1b97af31e44d1d6200c8
Sandi2156 <msandipan2016@gmail.com>
2021-06-21 20:35:08 +0530

    Change style of the page....

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ function Stopwatch(){
     let seconds = 0, running = false, startTime , totalTime , previous = 0;
 
     this.start = function(){
-        if(running)
+        if(running){
             document.getElementById('warning').innerHTML = `<h3>Stopwatch is alredy running</h3>`
+            setInterval(() => {
+                document.getElementById('warning').innerHTML =``;
+            }, 3000);
+        }
         
         else{
         running = true;
@@ -16,14 +20,18 @@ function Stopwatch(){
             setWatch.setSeconds(seconds);
             setWatch.setMinutes(seconds/60);
             setWatch.setHours(seconds/3600);
-            document.getElementById('stopwatch').innerHTML = `<span>${setWatch.getHours()} :: ${setWatch.getMinutes()} :: ${setWatch.getSeconds()}</span>`
+            document.getElementById('stopwatch').innerHTML = `<h3>${setWatch.getHours()} : ${setWatch.getMinutes()} : ${setWatch.getSeconds()}</h3>`
         }, 1000);
     }
     }
 
     this.stop = function(){
-        if(!running)
+        if(!running){
             document.getElementById('warning').innerHTML = `<h3>Stopwatch is alredy stopped</h3>`
+            setInterval(() => {
+                document.getElementById('warning').innerHTML =``;
+            }, 3000);
+        }
         else{
         previous = seconds;
         running = false;
@@ -36,6 +44,8 @@ function Stopwatch(){
         running = false;
         startTime = null;
         previous = 0;
+        document.getElementById('stopwatch').innerHTML = `<h3>00 : 00 : 00</h3>`
+
     }
 }
 
